b0c5089f6ce8051ad7fb3633cd5c812d5c5354f9
Allan <allanbp.br@gmail.com>
2020-02-12 17:22:31 +0000

    Initial commit

---
+++ universe.py
@@ -0,0 +1,92 @@
+import pygame
+from sys import exit
+from random import *
+import time
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+startcolors ={
+	0 : (255,255,255),
+	1 : (255,0,0),
+	2 : (0,0,255),
+	3 : (0,255,0),
+	4 : (randrange(0,255),randrange(0,255),randrange(0,255))}
+
+lehmer = 0
+galaxy_offset = [20000,0]
+def Lehmer():
+	global lehmer
+	lehmer += 0xe120fc15
+	tmp = lehmer*0x4a39b70d
+	m1 = (tmp>>32) ^ tmp
+	tmp = m1*0x12fad5c9
+	m2 = (tmp>>32) ^ tmp
+	return m2
+width = 800
+height = 800
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+resolution = 20
+SPEED = 100
+row = height//resolution
+col = width//resolution
+pygame.init()
+window = pygame.display.set_mode((width,height))
+fps = pygame.time.Clock()
+bro ={}
+ellapsedtime = 0
+w,z=0,0
+while(1):
+	w,z = 0,0
+	fps.tick(12)
+	ellapsedtime = pygame.time.get_ticks() - ellapsedtime
+	window.fill(pygame.Color(0,0,0))
+	for event in pygame.event.get():
+		if event.type==pygame.KEYDOWN:
+			if event.key == pygame.K_q:
+				pygame.quit()
+				exit()
+			if event.key == pygame.K_a:
+				galaxy_offset[0]-=SPEED
+			if event.key == pygame.K_d:
+				galaxy_offset[0]+=SPEED
+			if event.key == pygame.K_w:
+				galaxy_offset[1]-=SPEED
+			if event.key == pygame.K_s:
+				galaxy_offset[1]+=SPEED
+		if event.type == pygame.MOUSEBUTTONDOWN:
+			w,z = event.pos
+			w//=resolution
+			z//=resolution
+	start = time.time()
+	for i in range(row):
+		for j in range(col):
+			x,y = (i*resolution),(j*resolution)
+			x_coord,y_coord = x+galaxy_offset[0],y+galaxy_offset[1]
+			w_coord,z_coord = w*resolution,z*resolution
+			seed = (y_coord) << 16 | (x_coord)
+			lehmer = seed
+			color = Lehmer()%5
+			radius = Lehmer()%7 + 1
+			isS = Lehmer()%30 + 1 == 1
+			if(isS):
+				pygame.draw.circle(window, (255,255,255), (x+5,y+5), radius)
+				if(w_coord == x and z_coord==y):
+					if((w_coord,z_coord) not in bro.keys()):
+						bro[(w_coord,z_coord)] = "KNOW STAR"
+					#pygame.draw.circle(window, (255,0,0), ((w*resolution)+5,(z*resolution)+5), 10)
+					else:
+						#print(bro[(w_coord,z_coord)])
+						pass
+	end = time.time()
+
+	print(bcolors.OKBLUE + "Done" + bcolors.ENDC + "  " + bcolors.WARNING + str(end-start) + bcolors.ENDC)
+	pygame.display.flip()
